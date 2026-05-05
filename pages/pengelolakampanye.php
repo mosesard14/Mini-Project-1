e91701eb1301e0e8ec6c78f8a2bc53aa70c60516
71241099-ListyanAji <listyan.aji@ti.ukdw.ac.id>
2026-05-05 15:11:57 +0700

    update1

--- pages/pengelolakampanye.php
+++ pages/pengelolakampanye.php
@@ -1,26 +1,33 @@
+<?php
 session_start();
 include 'koneksi.php';
 if (!isset($_SESSION['username'])) {
     header("Location: login.php");
-    exit();
-}
-$action = isset($_GET['action']) ? $_GET['action'] : '';
-switch ($action) {
-    case 'form':
-        $id = isset($_GET['id']) ? $_GET['id'] : '';
-        echo "Form Pengelola Kampanye";
-        break;
-    case 'hapus':
-        $id = $_GET['id'];
-        $res=mysqli_query($koneksi, "SELECT total_dana FROM kampanye WHERE id_kampanye = '$id'");
-        $trg=mysqli_fetch_assoc($res);
-           if($trg['total_dana'] >= 10000) {echo "<script>alert('Kampanye tidak dapat dihapus karena sudah mencapai target dana');window.location.href='pengelolakampanye.php';</script>";} 
-           else {mysqli_query($koneksi, "DELETE FROM kampanye WHERE id_kampanye = '$id'");
-                 header("Location: pengelolakampanye.php");}
-        break;
-   
-        default:    
-        echo "Daftar Kampanye";
-        break;
-}
+    exit();}
+$id_pengelola = $SESSION["id_user"]
+$page = isset($_GET["page"]) ?
+$_GET["page"] : "list";
 ?>
+<!DOCTYPE html>
+<html>
+    <head><title>Pengelola Kampanye<title></head>
+    <body>
+        <header>
+            <nav>
+                <a href=></a>
+                <a href=></a>
+                <a href=></a>
+            <nav>
+        </header>
+       <main>
+           <?php
+                if (!isset($_SESSION['pesan'])) {
+                  echo "<p>" $_SESSION['pesan'] "<p>"; 
+                  unset($_SESSION['pesan'])
+                }
+                if ($page=='form'){include "view/form_kampanye.php;"}elseif($page=='donatur'){include "view/form_kampanye.php;"}else{include "view/form_kampanye.php;"}
+            ?>
+       </main>
+            
+    </body>
+</html>
