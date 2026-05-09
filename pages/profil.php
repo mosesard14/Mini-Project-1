32b9f220075e2f2a12ac0a7fb785ba58c7679a2d
mosesard14 <moses.putra@ti.ukdw.ac.id>
2026-05-09 15:41:21 +0700

    update sedikit bug

--- pages/profil.php
+++ pages/profil.php
@@ -253,7 +253,7 @@ if ($role === 'donatur') {
             <button type="submit" class="btn-save">Simpan Perubahan</button>
         </form>
 
-        <a href="logoout.php" class="btn-logout">
+        <a href="logout.php" class="btn-logout">
             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/><polyline points="16 17 21 12 16 7"/><line x1="21" y1="12" x2="9" y2="12"/></svg>
             Keluar dari Akun
         </a>
